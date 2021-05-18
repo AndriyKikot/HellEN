3999bd6b1b432e6ada213b1c228655a2a2bafc96
Andriy Kikot <andray838@gmail.com>
2021-05-18 22:37:29 +0300

    added popup and animation

--- src/form/mail.php
+++ src/form/mail.php
@@ -12,15 +12,15 @@ $phone = $_POST['phone'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  						  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'hell-en-form@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'hakhaton7'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'test@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'test'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('hell-en-form@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('zhyvotovskyiandrii@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('test@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('test@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
